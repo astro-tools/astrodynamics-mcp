70f717d735cca48952f6887edaf234b809089e38
Dimitrije Jankovic <djankov@gmail.com>
2026-05-26 23:32:51 -0400

    Cut v0.1.5: version bump, CHANGELOG

--- src/astrodynamics_mcp/__init__.py
+++ src/astrodynamics_mcp/__init__.py
@@ -5,7 +5,7 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("astrodynamics-mcp")
 except PackageNotFoundError:
-    __version__ = "0.1.4"
+    __version__ = "0.1.5"
 
 __all__ = ["__version__"]
 
